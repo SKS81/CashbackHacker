694dd085d2bbeb80024c91e527b9f8dec9ec5858
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-12-31 02:42:26 +0300

    Add CashbackHackServiceTest.java file

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        
+    }
+}
