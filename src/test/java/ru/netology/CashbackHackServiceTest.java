db7bdfb20eaadad7b179e5745b567f6a24a3cdd7
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-12-31 03:53:19 +0300

    Modified CashbackHackServiceTest.java file

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -4,7 +4,26 @@ import org.junit.Test;
 import org.junit.Assert;
 public class CashbackHackServiceTest {
 
+    CashbackHackService service = new CashbackHackService();
+
     @Test
-    public void remain() {
+    public void lacking100() {
+        int expected = 100;
+        int actual = service.remain(900);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lacking200() {
+        int expected = 200;
+        int actual = service.remain(2_800);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lacking0() {
+        int expected = 0;
+        int actual = service.remain(1_000);
+        Assert.assertEquals(expected, actual);
     }
 }
