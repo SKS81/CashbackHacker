076777a89edfff5d5b680f127c0265accf544fd6
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2023-01-03 21:39:19 +0300

    Modified CashbackHackServiceTest.java

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -22,8 +22,33 @@ public class CashbackHackServiceTest {
 
     @Test
     public void lacking0() {
-        int expected = 1_000;
+        int expected = 0;
         int actual = service.remain(1_000);
         Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void lacking0_Test() {
+        org.junit.Assert.assertEquals(0, service.remain(1_000));
+    }
+
+    @org.junit.Test
+    public void lacking500_Test() {
+        org.junit.Assert.assertEquals(500, service.remain(500));
+    }
+
+    @org.junit.Test
+    public void lacking1000_Test() {
+        org.junit.Assert.assertEquals(1_000, service.remain(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void lacking0_TestJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(0, service.remain(7_000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void lacking150_TestJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(150, service.remain(3_850));
     }
 }
