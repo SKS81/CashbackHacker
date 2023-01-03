9f6379696dae94db3a8329bbada9ee0a74041263
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2023-01-03 10:42:12 +0300

    Fixed bug

--- src/main/java/ru/netology/CashbackHackService.java
+++ src/main/java/ru/netology/CashbackHackService.java
@@ -4,6 +4,9 @@ public class CashbackHackService {
     private final int boundary = 1_000;
 
     public int remain(int amount) {
+        for (int i = 0; i <= amount; i += 1_000) {
+            if (i == amount) return 0;
+        }
         return boundary - amount % boundary;
     }
 }
